c82828bad69231a3e3a5e9667a2a127b29f50c2d
Jose Luis Soto Sanchez <jose.soto.sa@usach.cl>
2023-10-01 08:29:21 -0300

    creacion proyecto

---
+++ src/main/java/com/example/AppTopEducation/AppTopEducationApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppTopEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTopEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTopEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AppTopEducation/AppTopEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppTopEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTopEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
